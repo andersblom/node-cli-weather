741c19a75af08f0e8b7cb61c7743a5319639487a
Anders Blom <anders@welearn.dk>
2017-03-19 21:43:05 +0100

    doc and removing excess comments

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # node-cli-weather
-Powered by Dark Sky (https://darksky.net)
+
+Because why look out the window, when you can keep staring into your console?
+
+## Get started: 
+```
+node weather.js <your city here>
+
+
+Weather info powered by Dark Sky (https://darksky.net)
+
--- weather.js
+++ weather.js
@@ -1,11 +1,6 @@
 'use strict';
 
 const https = require("https");
-// Google Maps AIzaSyAWZrNTLzW03gIatwIk-2DpFMnckiYvcLU
-// https://maps.googleapis.com/maps/api/geocode/json?address=Odense%20Denmark&key=AIzaSyAWZrNTLzW03gIatwIk-2DpFMnckiYvcLU
-
-// Darksky 558592e8030e3dc0e74aca942f611848
-// https://api.darksky.net/forecast/558592e8030e3dc0e74aca942f611848/37.8267,-122.4233 (lat, long)
 
 // Convert request input from array to string
 const requestInput = process.argv.slice(2).join(" ");
